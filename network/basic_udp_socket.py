f4526bbe22ea883ff1bea5d35167763a7c83be0e
vincgonzo <vincgonzo@hotmail.fr>
2025-01-29 15:35:12 +0100

    dev(network): basic tcp/udp socket client

--- network/basic_udp_socket.py
+++ network/basic_udp_socket.py
@@ -3,7 +3,7 @@
 import socket
 
 target_host = "127.0.0.1"
-target_port = 80
+target_port = 9999 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # send some data

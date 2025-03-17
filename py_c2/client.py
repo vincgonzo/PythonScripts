c40e07aa8fb9906858d29beae5a293af9eb76905
vincgonzo <vincgonzo@hotmail.fr>
2025-03-17 17:01:45 +0100

    dev(c2): client refine

--- py_c2/client.py
+++ py_c2/client.py
@@ -1,7 +1,15 @@
 #!/bin/python3
 
+from requests import get
 
-import requests
+HEADER: dict[str, str] = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
 
-x = requests.get('http://localhost')
-print(x.text)
+# PROXY = {"https": "proxy.some-site.com:443"}
+PROXY = None
+
+x = get('http://localhost', headers=HEADER, proxies=PROXY)
+
+print(f"Response Object: {x.request.headers}\n\tHeader: {x.headers}\n\tReason: {x.reason}\n\tStatus: {x.status_code}")
+

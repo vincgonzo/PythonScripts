cf04846da65e4da46648c67f09c389a200e5941d
vincgonzo <vincgonzo@hotmail.fr>
2025-03-19 16:05:22 +0100

    dev(c2): server init(2)

--- py_c2/c2_server.py
+++ py_c2/c2_server.py
@@ -1,18 +1,26 @@
 #!/bin/python3
 
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
+PORT = 8900
+BIND_ADDR = ""
 
-from http.server import BaseHTTPRequestHandler as BHTTPR
-
-
-class C2Handler(BHTTPR):
+class C2Handler(BaseHTTPRequestHandler):
     """ This is a child class of the BaseHTTPRequestHandler class.
     It handles all HTTP request of our C2 server. """
+    
     
     # noinspection PyPep8Naming
     def do_GET(self):
         # first send back 404 to the client
         self.send_response(404)
         self.end_headers()
+
+server = HTTPServer((BIND_ADDR, PORT), C2Handler)
+server.serve_forever()
+# def run(server_class=HTTPServer, handler_class=BHTTPR):
+#     server_address = ('', 8900)
+#     httpd = server_class(server_address, handler_class)
+#     httpd.serve_forever()
 
 
--- py_c2/client.py
+++ py_c2/client.py
@@ -2,6 +2,8 @@
 
 from requests import get
 
+PORT=8900
+
 HEADER: dict[str, str] = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
@@ -9,7 +11,7 @@ HEADER: dict[str, str] = {
 # PROXY = {"https": "proxy.some-site.com:443"}
 PROXY = None
 
-x = get('http://localhost', headers=HEADER, proxies=PROXY)
+x = get(f'http://localhost:{PORT}', headers=HEADER, proxies=PROXY)
 
 print(f"Response Object: {x.request.headers}\n\tHeader: {x.headers}\n\tReason: {x.reason}\n\tStatus: {x.status_code}")
 

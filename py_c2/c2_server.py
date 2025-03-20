b829ea6a6d4b55507b19a9679eb15da2f8ecc70f
vincgonzo <vincgonzo@hotmail.fr>
2025-03-20 20:54:03 +0100

    dev(c2): server header change

--- py_c2/c2_server.py
+++ py_c2/c2_server.py
@@ -8,13 +8,18 @@ BIND_ADDR = ""
 class C2Handler(BaseHTTPRequestHandler):
     """ This is a child class of the BaseHTTPRequestHandler class.
     It handles all HTTP request of our C2 server. """
-    
+    server_version = "Apache/2.4.58"
+    sys_version = "(CentOS)"
     
     # noinspection PyPep8Naming
     def do_GET(self):
         # first send back 404 to the client
         self.send_response(404)
         self.end_headers()
+
+
+print("server version:", C2Handler.server_version)
+print("sys_version:", C2Handler.sys_version)
 
 server = HTTPServer((BIND_ADDR, PORT), C2Handler)
 server.serve_forever()
--- py_c2/client.py
+++ py_c2/client.py
@@ -3,15 +3,16 @@
 from requests import get
 
 PORT=8900
+PROXY = None
+C2_SERVER = "localhost"
 
 HEADER: dict[str, str] = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
 
 # PROXY = {"https": "proxy.some-site.com:443"}
-PROXY = None
 
-x = get(f'http://localhost:{PORT}', headers=HEADER, proxies=PROXY)
+x = get(f'http://{C2_SERVER}:{PORT}', headers=HEADER, proxies=PROXY)
 
 print(f"Response Object: {x.request.headers}\n\tHeader: {x.headers}\n\tReason: {x.reason}\n\tStatus: {x.status_code}")
 

